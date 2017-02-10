b2069e833bdf699b92a510f2e8eb8542668eccea
Laura Docherty <bunnylld@hotmail.com>
2017-02-10 13:57:29 +0000

    Code school steps 5 to 9

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,22 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(! (answer || attempt)){
+    	setHiddenFields();
+    }
 }
 
 //implement new functions here
+function setHiddenFields()
+{
+	answer = Math.floor(Math.random() * 9999);
+	answer = answer.toString();
+	while(answer.length < 4){
+		answer = "0" + answer;
+	}
+	attempt = 0;
+}
+
+function setMessage(text){
+	document.getElementById('message').innerHTML(test);
+}
