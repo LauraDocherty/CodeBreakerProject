e37d81aece9ff8f8642028b13fa3124635deeeba
Laura Docherty <bunnylld@hotmail.com>
2017-02-15 09:06:36 +0000

    Added getResults function

--- src/assets/main.js
+++ src/assets/main.js
@@ -42,5 +42,31 @@ return false
 }
 
 function getResults(input){
-	
+	var glyphs = '<div>';
+	for(var i =0; i++; i<input.length)
+	{
+		if(input.charAt(i) == answer.value.charAt(i))
+		{
+			glyphs = glyphs + '<span class="glyphicon glyphicon-ok"></span>';
+		}else 
+		{
+			var isThere = false;
+			for(var j=0; j++; j< input.length )
+			{
+					if(input.charAt(i) == answer.value.charAt(j))
+					{
+							glyphs = glyphs + '<span class="glyphicon glyphicon-transfer"></span>';
+							isThere = true;
+							break;
+					}
+			}
+			if(!isThere)
+			{
+				glyphs = glyphs + '<span class="glyphicon glyphicon-remove"></span>';	
+			}
+		}
+	}
+	glyphs = glyphs + '</div>';
+	var res = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">' + glyphs;
+	document.getElementById('results').innerHTML = res;
 }
